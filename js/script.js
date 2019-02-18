71ebfffd4662ce3c5338cf8dd8f2bcf9bebe4c01
Richard Hpa <091276@bun2091276m.local>
2019-02-18 15:25:37 +1300

    can group when even groups

--- js/script.js
+++ js/script.js
@@ -22,45 +22,22 @@ console.log(maxNumberOfGroups);
 
 $("#randomize").click(function(){
     if(people.length % maxNumberOfGroups === 0){
-        console.log('even');
-
-
-
-
-
-
-
-
-
         shuffle(remainingStudents);
         var shuffledStudents = shuffle(remainingStudents.slice());
-        console.log(shuffledStudents);
+
         var numberPerGroup = people.length/maxNumberOfGroups;
-        console.log(numberPerGroup);
+
         var j = 0;
-        // groups['group1'] = [];
+
         for (var i = 0; i < shuffledStudents.length; i++) {
             if(i % numberPerGroup === 0){
                 j++
                 groups['group'+(j)] = [];
             }
-            console.log('group'+j);
             groups['group'+j].push({
                 name: shuffledStudents[i].name
             })
         }
-        console.log(groups);
-
-
-
-
-
-
-
-
-
-
-
     } else {
         var evenGroups = false;
         console.log('odd');
